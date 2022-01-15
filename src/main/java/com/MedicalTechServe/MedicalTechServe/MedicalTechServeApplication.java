4f4dc0d2c1567ae361efcf3738b378a3ead66568
unknown <karthiks7678@gmail.com>
2022-01-15 06:17:42 -0800

    First Commit

---
+++ src/main/java/com/MedicalTechServe/MedicalTechServe/MedicalTechServeApplication.java
@@ -0,0 +1,13 @@
+package com.MedicalTechServe.MedicalTechServe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalTechServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalTechServeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MedicalTechServe/MedicalTechServe/MedicalTechServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MedicalTechServe.MedicalTechServe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalTechServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
